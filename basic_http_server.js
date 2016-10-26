04ac692f605b9780759d95027e80067a2dda51e9
coderrick <derrick.duncan@temple.edu>
2016-10-26 09:28:40 -0400

    update

--- basic_http_server.js
+++ basic_http_server.js
@@ -7,12 +7,14 @@
 
 var http = require('http');
 
+//Note client always makes 2 requests. Request for favicon is sent in the background?
 function onRequest(request, response){
-    console.log('A user made a request');
+    console.log('A user made a request' + request.url);
     response.writeHead(200, {"Context-Type":"text/plain"});
     response.write("Here is the response data");
     response.end();
 }
+
 //Create HTTP Server and have it listen on port 8888
 http.createServer(onRequest).listen(8888);
 console.log('Server is running on port 8888');
