5097e4ce04337f0eb56202094f12cfc8ee2328a5
coderrick <derrick.duncan@temple.edu>
2016-10-26 09:11:45 -0400

    Added code

--- basic_http_server.js
+++ basic_http_server.js
@@ -1,6 +1,18 @@
+/**
+ * 
+ * Credit to the thenewboston
+ * https://www.youtube.com/watch?v=pYOltVz7kL0
+ * 
+ */
+
 var http = require('http');
 
-
+function onRequest(request, response){
+    Console.log('A user made a request');
+    response.writeHead(200, ["Context-Type": "text/plain"]);
+    response.write("Here is the response data");
+    response.end();
+}
 //Create HTTP Server and have it listen on port 8888
 http.createServer(onRequest).listen(8888);
 console.log('Server is running on port 8888');
