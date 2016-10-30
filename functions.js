51979d0fcaac71b4c3a415e8ad849a19803f7a91
coderrick <derrick.duncan@temple.edu>
2016-10-29 23:17:14 -0400

    Added express + node_modules

--- functions.js
+++ functions.js
@@ -1,4 +1,6 @@
 /**
+ * Types of Javascript Functions: Regular, Anonymous, IIFE, Method, and Callbacks/Closures
+ * 
  * Function Expression - var test = function() {};
  * Function Declaration - function test() {};
  * 
@@ -9,15 +11,15 @@
  * Regular function example
  */
 function showOutput(){
-    console.log('I am a regular function');
+    console.log('I\'m a regular function');
 }
 
 /**
  * Anonymous function example
  */
 var a = function (){
-    console.log('I am a regular function');
-}
+    console.log('I\'m an anonymous function');
+}//() Add that to make the function execute immediately 
 
 /**
  * http://gregfranko.com/blog/i-love-my-iife/ 
@@ -25,7 +27,7 @@ var a = function (){
  * wrapping function in parenthesis turns it into a function express see top of file
  */
 (function(){
-  // my special code
+  console.log('I\'m an IIFE function');
 }());
 
 /** 
@@ -35,15 +37,20 @@ var a = function (){
  */
 var anObject = {
     method: function justDoIt(){
-        console.log('I am a method');
+        console.log('I\'m a method');
     }
 }
 
 
 /**
- * 
+ * http://javascriptissexy.com/understand-javascript-callback-functions-and-use-them/
  * A callback function, also known as a higher-order function, is a function that is passed to another 
  * function (let’s call this other function “otherFunction”) as a parameter, and the callback function is called
  * (or executed) inside the otherFunction
  * 
  */
+
+
+showOutput();
+a;
+anObject.method();
