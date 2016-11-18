58c0479f6dd141adcf9d03bc9eaa4a9b21c8fa26
coderrick <derrick.duncan@temple.edu>
2016-11-18 15:19:37 -0500

    Added file write code

--- file_write.js
+++ file_write.js
@@ -1,1 +1,11 @@
-//soon
+/**
+ * http://stackoverflow.com/questions/2496710/writing-files-in-node-js
+ */
+
+var fs = require('fs');
+var stream = fs.createWriteStream("my_file.txt");
+stream.once('open', function(fd) {
+  stream.write("My first row\n");
+  stream.write("My second row\n");
+  stream.end();
+});
