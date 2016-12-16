87ed92d36e10d4e3654dc5a870a32dad5fd102fd
coderrick <derrick.duncan@temple.edu>
2016-12-16 00:00:00 -0500

    updates

--- express_demo/testapp/app.js
+++ express_demo/testapp/app.js
@@ -7,6 +7,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+require("nodejs-dashboard");
 
 var index = require('./routes/index');
 var users = require('./routes/users');
